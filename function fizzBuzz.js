bf131ae68ae92bfd5275ca9b5f551efc97957c36
J A S SANTHOSH <148249420+jassanthosh@users.noreply.github.com>
2024-10-07 07:37:21 +0530

    Add files via upload

---
+++ function fizzBuzz.js
@@ -0,0 +1,16 @@
+function fizzBuzz(start, end) {
+  for (let i = start; i <= end; i++) {
+    if (i % 15 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);   
+
+    }
+  }
+}
+
+fizzBuzz(1, 15);
